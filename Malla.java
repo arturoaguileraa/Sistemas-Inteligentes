a2dfba0c666f562f4458b47bb466d1d93c429409
Arturo Aguilera <arturoaguilera@uma.es>
2021-03-31 00:11:13 +0200

    Modificación Hito 1

--- Malla.java
+++ Malla.java
@@ -23,69 +23,85 @@ public class Malla {
 		numObstaculos = nObs;
 		
 		int matriz[][] = new int [nf][nc];
+		iniciaracero(matriz, nf, nc);
 		meterObstaculos(matriz, rnd);
 
         this.matriz = matriz;
 		
-	}
-		
-	public void ver (Malla a) {
+	}	
 
-		for(int i=0; i < numfilas; i++){
-			for(int j=0; j < numColumnas; j++){
+	protected void iniciaracero(int matriz[][], int nf, int nc){
+		for(int i=0; i<nf; i++){
+			for(int j=0; j<nc; i++){
+				matriz[i][j] = 0;
+			}
+		}
+	}
+	
+	
+	protected void meterObstaculos(int [][] matriz, Random rnd){
+ 		int counter = 0;
+    	int posIi, posIj, posFi, posFj;
+	
+		while (counter < numObstaculos){
+			 int lugarfila = rnd.nextInt(numfilas);
+			 int lugarcolumna = rnd.nextInt(numColumnas);
+
+			 if(matriz[lugarfila][lugarcolumna]!=1){
+        	    matriz[lugarfila][lugarcolumna]=1;
+				counter++;
+			 }
+		}
+
+
+		int ii, ij, fi, fj;
+		do{
+			ii = rnd.nextInt(numfilas);
+			ij = rnd.nextInt(numColumnas);
+			fi = rnd.nextInt(numfilas);
+			fj = rnd.nextInt(numColumnas);
+			if ((matriz[ii][ij] != 1) && (matriz[fi][fj] != 1)){
+				posInii = ii;
+				posInij = ij;
+				posFini = fi;
+				posFinj = fj;
+			}
+		}while((matriz[ii][ij] == 1) || (matriz[fi][fj] == 1));
+
+        
+	}
+
+    public void ver (Malla a) {
+		System.out.println("Posicion inicial: (" + a.posInii + ", " + a.posInij + ")");
+		System.out.println("Posicion final: (" + a.posFini + ", " + a.posFinj + ")");
+		
+		System.out.println("Malla: ");
+		System.out.println();
+		for(int i=0; i < a.numfilas; i++){
+			for(int j=0; j < a.numColumnas; j++){
 				
-				System.out.println(matriz[i][j]);
+				System.out.println(a.matriz[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
 
-	public void meterObstaculos(int [][] matriz, Random rnd){
- 	int counter = 0;
-    int posIi, posIj, posFi, posFj;
-	
-	 while (counter < numObstaculos){
-		 int lugarfila = rnd.nextInt(numfilas);
-		 int lugarcolumna = rnd.nextInt(numColumnas);
-
-		 if(matriz[lugarfila][lugarcolumna]!=1){
-            matriz[lugarfila][lugarcolumna]=1;
-			counter++;
-		 }
-		}
-
-		int ii = rnd.nextInt(numfilas);
-		int ij = rnd.nextInt(numColumnas);
-		int fi = rnd.nextInt(numfilas);
-		int fj = rnd.nextInt(numColumnas);
-		if ((matriz[ii][ij] != 1) && (matriz[fi][fj] != 1)){
-			posInii = ii;
-			posInij = ij;
-			posFini = fi;
-			posFinj = fj;
-		}
-
-
-
-
-
-
-        int i = rnd.nextInt(numfilas);
-		int j = rnd.nextInt(numColumnas);
-        
-        while (posIi )
-
-
-
-       
-        
+	public int[][] getMalla(){
+		return matriz;
 	}
-    
-    public void posIni (){
-		return System.out.println("Posicion inicial: (" ++ posInii ++ ", " ++ posInij ++ ")");
+
+    public int[] getposIni() {
+		int[] posIni = new int[2];
+		posIni[0] = posInii;
+		posIni[1] = posInij;
+		return posIni;
 	}
-	public void posFin(){
-		
-	} 
+
+	public int[] getposFin() {
+		int[] posFin = new int[2];
+		posFin[0] = posFini;
+		posFin[1] = posFinj;
+		return posFin;
+	}
 
 }
