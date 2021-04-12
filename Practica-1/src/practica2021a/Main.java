2985ef8a02457acba5bfb78eb0cecad271178947
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-04-12 14:21:12 +0200

    Main Init

--- Practica-1/src/practica2021a/Main.java
+++ Practica-1/src/practica2021a/Main.java
@@ -1,15 +1,24 @@
 package practica2021a;
 
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     
     public static void main(String[] args) {
-         Malla malla = new Malla(27, 10, 10, 25);
+         Malla malla = new Malla(24, 10, 10, 97);
          Estado es = new EstadoMalla(malla, malla.getposIni()[0], malla.getposIni()[1]);
          Estado eo = new EstadoMalla(malla, malla.getposFin()[0], malla.getposFin()[1]);
 
+         System.out.println("POSICION INICIAL" + Arrays.toString(malla.getposIni()));
+         System.out.println("POSICION FINAL" + Arrays.toString(malla.getposFin()));
+         
          AgenteA prueba = new AgenteA();
-         System.out.println(prueba.resuelve(es, eo).toString());
+         List<Estado> solucion = prueba.resuelve(es, eo);
+         System.out.println("\n Camino Solucion \n");
 
+         for (int i=solucion.size()-1; i>=0; i--) {
+            solucion.get(i).ver();
+        }
     }
 }
